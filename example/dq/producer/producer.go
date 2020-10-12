719f2e813cd2b4df73f7168e4e62edb6e79c9702
sjatsh <i@sjis.me>
2020-10-12 17:51:22 +0800

    add dq and kq example

---
+++ example/dq/producer/producer.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+    "github.com/tal-tech/go-queue/dq"
+)
+
+func main() {
+	producer := dq.NewProducer([]dq.Beanstalk{
+		{
+			Endpoint: "localhost:11300",
+			Tube:     "tube",
+		},
+		{
+			Endpoint: "localhost:11300",
+			Tube:     "tube",
+		},
+	})
+	for i := 1000; i < 1005; i++ {
+		_, err := producer.Delay([]byte(strconv.Itoa(i)), time.Second*5)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
