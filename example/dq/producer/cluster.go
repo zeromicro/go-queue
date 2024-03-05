4d3c427b9b91662ce70ae4019ab49f47e1e7d986
ch3nnn <chenyinren@vip.qq.com>
2024-03-05 16:13:51 +0800

    fix: producerNode.Delay func not wrap()

--- example/dq/producer/cluster.go
+++ example/dq/producer/cluster.go
@@ -9,8 +9,16 @@ import (
 )
 
 func main() {
-	producer := dq.NewProducerNode("localhost:11300", "tube")
-
+	producer := dq.NewProducer([]dq.Beanstalk{
+		{
+			Endpoint: "localhost:11300",
+			Tube:     "tube",
+		},
+		{
+			Endpoint: "localhost:11300",
+			Tube:     "tube",
+		},
+	})
 	for i := 1000; i < 1005; i++ {
 		_, err := producer.Delay([]byte(strconv.Itoa(i)), time.Second*5)
 		if err != nil {
