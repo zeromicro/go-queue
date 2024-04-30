00dd2b0296b8324b543f4159e2d47aeecd2598f2
kevin <wanjunfeng@gmail.com>
2024-04-30 22:50:33 +0800

    feat: commit in order

--- kq/config.go
+++ kq/config.go
@@ -9,17 +9,18 @@ const (
 
 type KqConf struct {
 	service.ServiceConf
-	Brokers     []string
-	Group       string
-	Topic       string
-	CaFile      string `json:",optional"`
-	Offset      string `json:",options=first|last,default=last"`
-	Conns       int    `json:",default=1"`
-	Consumers   int    `json:",default=8"`
-	Processors  int    `json:",default=8"`
-	MinBytes    int    `json:",default=10240"`    // 10K
-	MaxBytes    int    `json:",default=10485760"` // 10M
-	Username    string `json:",optional"`
-	Password    string `json:",optional"`
-	ForceCommit bool   `json:",default=true"`
+	Brokers       []string
+	Group         string
+	Topic         string
+	CaFile        string `json:",optional"`
+	Offset        string `json:",options=first|last,default=last"`
+	Conns         int    `json:",default=1"`
+	Consumers     int    `json:",default=8"`
+	Processors    int    `json:",default=8"`
+	MinBytes      int    `json:",default=10240"`    // 10K
+	MaxBytes      int    `json:",default=10485760"` // 10M
+	Username      string `json:",optional"`
+	Password      string `json:",optional"`
+	ForceCommit   bool   `json:",default=true"`
+	CommitInOrder bool   `json:",default=false"`
 }
